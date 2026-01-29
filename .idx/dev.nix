b76886d11a08e1fac49781cb8286f204b0ea8619
Nadun Bothota <d.nadunbothota@gmail.com>
2026-01-29 16:33:45 +0530

    Update Nix channel to stable-24.05

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,8 +1,8 @@
 
 {pkgs}: {
-  # Which nixpkgs channel to use.
+ 
   channel = "stable-24.05"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
+ 
   packages = [
     pkgs.php82
     pkgs.php82Packages.composer
